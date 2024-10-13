286589dd12702589308b7f2f7d1d33790b83dc9f
Drexler Mora <morajohndrexler@gmail.com>
2024-10-13 21:31:15 +0800

    Saving Data using JPA repository

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,8 +1,27 @@
 package com.example.demo.student;
 
 import java.time.LocalDate;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 
+@Entity
+@Table
 public class Student {
+
+    @Id
+    @SequenceGenerator(
+        name = "student_sequence",
+        sequenceName = "student_sequence",
+        allocationSize = 1
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "student_sequence"
+    )
 
     private Long id;
     private String name;
@@ -11,7 +30,6 @@ public class Student {
     private Integer age;
 
     public Student(){
-        
     }
 
     public Student(Long id, String name, String email, LocalDate dob, Integer age) {
